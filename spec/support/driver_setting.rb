b6a58329b4278bbecba60c0cd2c9178d9ce9a884
hiro266 <t1freejob@gmail.com>
2020-04-07 14:25:58 +0900

    Spec実行時、ブラウザの挙動をコメント

--- spec/support/driver_setting.rb
+++ spec/support/driver_setting.rb
@@ -1,7 +1,11 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     # driven_by(:rack_test)
+    
+    # Spec実行時、ブラウザが自動で立ち上がり挙動を確認できる
     # driven_by(:selenium_chrome)
+
+    # Spec実行時、ブラウザOFF
     driven_by(:selenium_chrome_headless)
   end
 end
